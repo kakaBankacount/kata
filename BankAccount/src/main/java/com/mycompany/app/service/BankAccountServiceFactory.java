2bb9451e5fc03be5b026eebc5b414caa58f9ee72
shopizertest <shopizertest@hotmail.com>
2017-03-13 01:16:06 +0100

    m20
    # Conflicts:
    #	BankAccount/src/main/java/com/mycompany/app/service/BankAccountServiceFactory.java

--- BankAccount/src/main/java/com/mycompany/app/service/BankAccountServiceFactory.java
+++ BankAccount/src/main/java/com/mycompany/app/service/BankAccountServiceFactory.java
@@ -1,5 +1,9 @@
 aapackage com.mycompany.app.service;
+<<<<<<< HEAD
 abccccsdefghijkl
+=======
+abccccsdefghij
+>>>>>>> b9c1d01... m20
 public class BankAccountServiceFactory {
 	
 	public static IBankService createBankAccountService() {
